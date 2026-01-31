7b5894a069f7ef78dc5173f7961860f917460c13
Luca Lucidera <Luca Lucidera@DKR-PC-LUCA>
2026-01-31 03:22:32 +0100

    Aggiunto kill y e rimosso Main.cs

---
+++ Assets/Scripts/Runtime/KillY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Runtime
+{
+	[RequireComponent(typeof(BoxCollider))]
+	public class KillY: MonoBehaviour
+	{
+        void Awake()
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
